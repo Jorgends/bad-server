bb11d8ca0659928bd5ceb1ad7050ab8736794408
Jorgend <Fullsteck@yandex.kz>
2026-07-11 14:21:56 +0500

    0.0.1

--- backend/src/controllers/customers.ts
+++ backend/src/controllers/customers.ts
@@ -10,7 +10,6 @@ import sanitizeHtml from '../utils/sanitizeHtml'
 const MAX_PAGE_SIZE = 100
 const MAX_SEARCH_LENGTH = 100
 
-// TODO: Добавить guard admin
 // eslint-disable-next-line max-len
 // Get GET /customers?page=2&limit=5&sort=totalAmount&order=desc&registrationDateFrom=2023-01-01&registrationDateTo=2023-12-31&lastOrderDateFrom=2023-01-01&lastOrderDateTo=2023-12-31&totalAmountFrom=100&totalAmountTo=1000&orderCountFrom=1&orderCountTo=10
 export const getCustomers = async (
@@ -291,7 +290,6 @@ export const getCustomers = async (
     }
 }
 
-// TODO: Добавить guard admin
 // Get /customers/:id
 export const getCustomerById = async (
     req: Request,
@@ -320,7 +318,6 @@ export const getCustomerById = async (
     }
 }
 
-// TODO: Добавить guard admin
 // Patch /customers/:id
 export const updateCustomer = async (
     req: Request,
@@ -375,7 +372,6 @@ export const updateCustomer = async (
     }
 }
 
-// TODO: Добавить guard admin
 // Delete /customers/:id
 export const deleteCustomer = async (
     req: Request,
